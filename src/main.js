a15898a100a256b6252bb8bcf38977d1af8713d3
loulp <louislpogam@orange.fr>
2022-11-03 12:12:42 +0100

    test cloudinary

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from './router';
+import Cloudinary, { CldImage } from 'cloudinary-vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Cloudinary, {
+  configuration: {
+    cloudName: "dtrcg7rwu",
+    secure: true,
+  },
+  components: { CldImage },
+});
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -6,8 +6,39 @@
 
 <script>
 import mediaList from "../components/gallery/mediaList.vue";
+import axios from "axios";
+
 export default {
   components: { mediaList },
+
+  data() {
+    return {
+      mediaList: [],
+    };
+  },
+
+  mounted() {
+    this.getMedia();
+  },
+
+  methods: {
+    getMedia() {
+      axios
+        .get("https://api.cloudinary.com/v1_1/dtrcg7rwu/image", {
+          headers: {
+            Authorization: `Basic 182242937219763:VPDWqG7D3meMJo0t9NAbfekA7nI`,
+          },
+        })
+        .then(
+          (res) => {
+            console.log(res);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+    },
+  },
 };
 </script>
 
